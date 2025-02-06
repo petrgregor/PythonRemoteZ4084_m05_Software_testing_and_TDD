e16c1145fe488939a6cbaad1bae7eba300dd0842
Petr Gregor <petr.gregor@email.cz>
2025-02-06 20:49:19 +0100

    BasicCalculator - part 2.

--- p01_BasicCalculator.py
+++ p01_BasicCalculator.py
@@ -12,11 +12,23 @@ class BasicCalculator:
             return num1 - num2
         return None
 
+    def multiply(self, num1, num2):
+        if ((isinstance(num1, int) or isinstance(num1, float)) and
+                (isinstance(num2, int) or isinstance(num2, float))):
+            return num1 * num2
+        return None
+
+    def divide(self, num1, num2):
+        if ((isinstance(num1, int) or isinstance(num1, float)) and
+                (isinstance(num2, int) or isinstance(num2, float))):
+            return num1 / num2
+        return None
+
 
 if __name__ == '__main__':
     basic_calculator = BasicCalculator()
     #num1 = int(input("Zadej první číslo: "))
     #num2 = int(input("Zadej druhé číslo: "))
-    num1 = "5"
-    num2 = "3"
-    print(f"{num1} + {num2} = {basic_calculator.add(num1, num2)}")
+    num1 = 5
+    num2 = 0
+    print(f"{num1} + {num2} = {basic_calculator.divide(num1, num2)}")
--- p01_BasicCalculator_test.py
+++ p01_BasicCalculator_test.py
@@ -32,3 +32,39 @@ def test_subtraction():
     assert basic_calculator.subtract("Hello", "World") is None
     assert basic_calculator.subtract(3, "5") is None
     assert basic_calculator.subtract([3], 5) is None
+
+
+def test_multiply():
+    basic_calculator = BasicCalculator()
+    result = basic_calculator.multiply(3, 5)
+    assert result == 15
+    assert basic_calculator.multiply(-6, -4) == 24
+    assert basic_calculator.multiply(-3, 4) == -12
+    assert basic_calculator.multiply(5, -6) == -30
+    assert basic_calculator.multiply(5, 0) == 0
+    assert round(basic_calculator.multiply(2.1, 3.2), 5) == 6.72
+    assert round(basic_calculator.multiply(10, 5.3), 5) == 53
+    assert basic_calculator.multiply("3", "5") is None
+    assert basic_calculator.multiply("Hello", "World") is None
+    assert basic_calculator.multiply(3, "5") is None
+    assert basic_calculator.multiply([3], 5) is None
+
+
+def test_divide():
+    basic_calculator = BasicCalculator()
+    result = basic_calculator.divide(6, 3)
+    assert result == 2
+    assert basic_calculator.divide(-6, -2) == 3
+    assert basic_calculator.divide(-3, 3) == -1
+    assert basic_calculator.divide(6, -6) == -1
+    assert round(basic_calculator.divide(2.1, 3.2), 5) == 0.65625
+    assert round(basic_calculator.divide(11.25, 2.25), 5) == 5
+    assert basic_calculator.divide("3", "5") is None
+    assert basic_calculator.divide("Hello", "World") is None
+    assert basic_calculator.divide(3, "5") is None
+    assert basic_calculator.divide([3], 5) is None
+
+    # assert basic_calculator.divide(5, 0) == 5
+    # testování výjimky
+    with pytest.raises(Exception):
+        assert basic_calculator.divide(5, 0)
